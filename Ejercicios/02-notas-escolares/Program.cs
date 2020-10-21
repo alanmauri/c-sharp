def58d0a22cd347b61c180521c737b9517bc59f9
Alan Cornejo <cppcornejo.castro94@gmail.comcpp>
2020-10-21 17:43:53 -0600

    Notas de escolares

---
+++ Ejercicios/02-notas-escolares/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace notas_escolares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno Alumno1 = new Alumno();
+            Alumno1.codigo = 1;
+            Alumno1.nombre = "Juan";
+            Alumno1.ingresarNota();
+
+            Alumno Alumno2 = new Alumno();
+            Alumno2.codigo = 2;
+            Alumno2.nombre = "Gustavo";
+            Alumno2.ingresarNota();
+
+            Console.WriteLine("Alumno: " + Alumno1.nombre);
+            Console.WriteLine("Alumno: " + Alumno2.nombre);
+        }
+    }
+}
---
+++ Ejercicios/02-notas-escolares/alumnos.cs
@@ -0,0 +1,8 @@
+public class Alumno
+{
+    public int codigo { get; set; }
+    public string nombre { get; set; }
+    void ingresarNota(){
+        
+    }
+}
